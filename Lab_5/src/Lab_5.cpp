18f9bebded68c6c88fb1b0d0da7c35ec6fc249a0
Nguyen Hanh <nthonghanh1412@gmail.com>
2021-04-13 22:11:31 +0300

    Lab 5 - done

--- Lab_5/src/Lab_5.cpp
+++ Lab_5/src/Lab_5.cpp
@@ -8,32 +8,56 @@
 
 #include <iostream>
 #include <vector>
-#include <algorithm> //std::sort
-#include <stdlib.h> //std::rand
-#include <cmath> //std::floor
+#include <algorithm>
+
 using namespace std;
 
-int binSearch(int num, vector<int> numArr){
-	int l = 0;
-	int r = numArr.size() - 1;
-	while(l <= r){
-		int mid = floor((l+r)/2);
-		if (numArr[mid] < num)
-			l = mid + 1;
-		else if (numArr[mid] > num)
-			r =  mid -1;
-		else
-			return mid;
-	}
-	return -1;
+int binSearch(vector<int> numArr, int num, int l, int r){
+    if(r >=l ){
+        int mid = l + (r - l) / 2;
+
+        if(numArr[mid] == num)
+        	return mid;
+
+        if (numArr[mid] > num)
+        	return binSearch(numArr, num, l, mid - 1);
+
+        return binSearch(numArr, num, mid + 1, r);
+    }
+
+    return -1;
 }
 
 
-int main() {
-	int num;
-	cout << "Enter a number: " << endl;
-	cin >> num;
-	vector<int>numArr(100);
+vector<int> indexSearch(vector<int> numArr, int num, int l, int r){
+	vector<int> indexArr;
+	int x = binSearch(numArr, num, l, r);
+	if (x == -1)
+	{
+		indexArr.push_back(x);
+		return indexArr;
+	}
+	else {
+		int i = 0;
+		while(numArr[x - i] == num){
+			indexArr.push_back(x-i);
+			i++;
+		}
+
+		i = 1;
+		while(numArr[x + i] == num){
+			indexArr.push_back(x+i);
+			i++;
+		}
+		sort(indexArr.begin(), indexArr.end());
+		return indexArr;
+	}
+}
+
+
+int main(){
+	cout << "Array of numbers: ";
+	vector<int> numArr(100);
 	for(int i=0; i<100; i++){
 		numArr[i] = rand() %100; // numArr in the range 0 to 99
 	}
@@ -42,12 +66,18 @@ int main() {
 		cout << numArr[i] <<" ";
 	}
 	cout << endl;
-	int x =  binSearch(num, numArr);
-	if(x==-1)
-		cout << "Not found";
-	else
-		cout << "Found";
 
+	int num;
+	cout << "Enter a number: ";
+	cin >> num;
 
-	return 0;
+	int n = numArr.size();
+
+	vector<int> indexArr = indexSearch(numArr,num, 0, n-1);
+	cout << "Index Array: [ ";
+	for (int i = 0; i < (int)indexArr.size(); i++){
+		cout << indexArr.at(i) << " ";
+	}
+	cout << "]";
+    return 0;
 }
